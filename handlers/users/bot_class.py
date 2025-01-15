ba196732d8d5ea6d606a0a352a99c7fb3c5936ec
Mur0dDev <msadullaevdev@gmail.com>
2025-01-15 13:36:36 -0500

    commented unneeded file codes

--- handlers/users/bot_class.py
+++ handlers/users/bot_class.py
@@ -1,62 +1,62 @@
-from handlers.users.classify import start_classification, process_being_type, process_human_data  # Import existing handlers
-from utils.db_api.google_sheets import GoogleSheetsClient  # Import Google Sheets client
-from aiogram import Bot, Dispatcher
-
-class BeingClassifierBot:
-    def __init__(self, bot, dp):
-        """
-        Initialize the bot, dispatcher, and Google Sheets client.
-        """
-        self.bot = bot
-        self.dp = dp
-        self.google_sheets = GoogleSheetsClient(
-            credentials_file="credentials.json",
-            spreadsheet_name="Being Classification Data"
-        )
-
-    def register_handlers(self):
-        """
-        Register all handlers with the dispatcher.
-        """
-        # Command handlers
-        self.dp.register_message_handler(start_classification, commands=["classify"])
-
-        # Callback query handlers
-        self.dp.register_callback_query_handler(process_being_type, lambda call: call.data.startswith("type_"))
-
-        # Human flow handlers
-        self.dp.register_message_handler(process_human_data, state="*")
-
-        # Animal flow handlers
-        self.dp.register_message_handler(self.process_animal_data, state="*")
-
-        # Alien flow handlers
-        self.dp.register_message_handler(self.process_alien_data, state="*")
-
-    def process_animal_data(self, update, context):
-        """
-        Handle animal classification flow.
-        """
-        # Placeholder logic for animals
-        pass
-
-    def process_alien_data(self, update, context):
-        """
-        Handle alien classification flow.
-        """
-        # Placeholder logic for aliens
-        pass
-
-    def save_to_sheets(self, data):
-        """
-        Save the classified data to Google Sheets.
-        """
-        self.google_sheets.authenticate()
-        self.google_sheets.append_data("Humans", data)
-
-    def post_to_group(self, data):
-        """
-        Post the classification results to a Telegram group.
-        """
-        pass
-
+# from handlers.users.classify import start_classification, process_being_type, process_human_data  # Import existing handlers
+# from utils.db_api.google_sheets import GoogleSheetsClient  # Import Google Sheets client
+# from aiogram import Bot, Dispatcher
+#
+# class BeingClassifierBot:
+#     def __init__(self, bot, dp):
+#         """
+#         Initialize the bot, dispatcher, and Google Sheets client.
+#         """
+#         self.bot = bot
+#         self.dp = dp
+#         self.google_sheets = GoogleSheetsClient(
+#             credentials_file="credentials.json",
+#             spreadsheet_name="Being Classification Data"
+#         )
+#
+#     def register_handlers(self):
+#         """
+#         Register all handlers with the dispatcher.
+#         """
+#         # Command handlers
+#         self.dp.register_message_handler(start_classification, commands=["classify"])
+#
+#         # Callback query handlers
+#         self.dp.register_callback_query_handler(process_being_type, lambda call: call.data.startswith("type_"))
+#
+#         # Human flow handlers
+#         self.dp.register_message_handler(process_human_data, state="*")
+#
+#         # Animal flow handlers
+#         self.dp.register_message_handler(self.process_animal_data, state="*")
+#
+#         # Alien flow handlers
+#         self.dp.register_message_handler(self.process_alien_data, state="*")
+#
+#     def process_animal_data(self, update, context):
+#         """
+#         Handle animal classification flow.
+#         """
+#         # Placeholder logic for animals
+#         pass
+#
+#     def process_alien_data(self, update, context):
+#         """
+#         Handle alien classification flow.
+#         """
+#         # Placeholder logic for aliens
+#         pass
+#
+#     def save_to_sheets(self, data):
+#         """
+#         Save the classified data to Google Sheets.
+#         """
+#         self.google_sheets.authenticate()
+#         self.google_sheets.append_data("Humans", data)
+#
+#     def post_to_group(self, data):
+#         """
+#         Post the classification results to a Telegram group.
+#         """
+#         pass
+#
